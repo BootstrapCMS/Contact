4b5da0795dc3546d4445c28acc4ed24f2a0b6556
Graham Campbell <graham@alt-three.com>
2015-06-25 14:09:41 +0100

    Updated email address

--- src/Mailer.php
+++ src/Mailer.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Laravel Contact.
  *
- * (c) Graham Campbell <graham@cachethq.io>
+ * (c) Graham Campbell <graham@alt-three.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,7 +16,7 @@ use Illuminate\Contracts\Mail\MailQueue as Mail;
 /**
  * This is the contact mailer class.
  *
- * @author Graham Campbell <graham@cachethq.io>
+ * @author Graham Campbell <graham@alt-three.com>
  */
 class Mailer
 {
